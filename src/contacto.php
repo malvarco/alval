ac42be511fe8809df6747290d469869994658881
Manuel Alvarez <malvarco@hotmail.com>
2020-10-15 00:07:00 -0300

    contacto template file incluye amp php procesador de mail funcionando

--- src/contacto.php
+++ src/contacto.php
@@ -4,6 +4,34 @@
  */
 ?>
 
+<?php
+if(!empty($_POST)){
+$name=$_POST['nombre'];
+$escuela=$_POST['escuela'];
+$email = $_POST['correo'];
+$phone = $_POST['fono'];
+$enquiry = $_POST['asunto'];
+$mensaje= $_POST['mensaje'];
+$formcontent=" De: $name \n Escuela: $escuela \n Teléfono: $phone \n **************************** \n Mensaje:\n $mensaje";
+$recipient = "avaldenegro@alval.cl";
+$subject = $enquiry;
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+
+       $domain_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+        header("Content-type: application/json");
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Origin: ". str_replace('.', '-','https://www.alval.cl') .".cdn.ampproject.org");
+        header("AMP-Access-Control-Allow-Source-Origin: " . $domain_url);
+        header("Access-Control-Expose-Headers: AMP-Access-Control-Allow-Source-Origin");
+        header("AMP-Redirect-To: https://www.alval.cl");
+        header("Access-Control-Expose-Headers: AMP-Redirect-To, AMP-Access-Control-Allow-Source-Origin"); 
+        echo json_encode(array('name' => $name));
+        exit;
+}
+?>
+
+
 <div class="<?= $page->title ?>">
 
 
@@ -12,6 +40,7 @@ $etiqueta = str_getcsv($page->etiquetas, "\n");
 ?>   
 
 <div class="block-contacto">
+<a id="<?= $page->anchor ?>"></a>
     <div class="image-contacto">
     <amp-img class="contain" alt="Aldo Valdenegro" height="450" width="450" layout="fixed" src="<?= $page->images->get('aldo_valdenegro.jpg')->url; ?>" >
     </div>
@@ -20,10 +49,11 @@ $etiqueta = str_getcsv($page->etiquetas, "\n");
 
 </div>
 
+
 <div class="form-container">
     <form class="contact-box"
       method="post"
-      action-xhr="/documentation/examples/api/submit-form-input-text-xhr"
+      action-xhr="<?= $page->url ?>"
       target="_top">
             <div class="input-fields">
             <input type="text"
@@ -41,7 +71,7 @@ $etiqueta = str_getcsv($page->etiquetas, "\n");
             name="correo"
             placeholder="<?= $etiqueta[2]; ?>"
             required>
-            <input type="text"
+            <input type="tel"
             class="input-text"
             name="fono"
             placeholder="<?= $etiqueta[3]; ?>"
@@ -60,18 +90,9 @@ $etiqueta = str_getcsv($page->etiquetas, "\n");
             required></textarea>
             <input class="boton-enviar" type="submit" value="<?= $etiqueta[6]; ?>">
             </div>
-<div class="respuestas" submit-success>
-    <template type="amp-mustache">
-     <?= $etiqueta[7]; ?> 
-    </template>
-  </div>
-  <div class="respuestas" submit-error>
-    <template type="amp-mustache">
-¡Ha RRUURRRocurrido {{nombre}} un error!
-    </template>
-  </div>
     </form>
 </div>
+
 
 
 </div>
